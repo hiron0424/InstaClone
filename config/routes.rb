080d5794ec66124d34fe7b254242043bcdd840d8
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-03-26 12:18:45 +0900

    ユーザ登録機能、ログイン、ログアウト機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root to: 'photos#index'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :photos do
     collection do
       post :confirm
     end
   end
+
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
