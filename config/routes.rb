e4e92b0bf41ec6f4333ff09f74b3e3a302a9e883
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-03-27 15:03:56 +0900

    プロフィール画像登録機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:new, :create, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
